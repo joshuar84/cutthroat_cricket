c934df4f10ed844c82c0e22f24fb1624b5347976
Joshua E. Rios <1984yearoftherat@gmail.com>
2021-01-26 23:32:43 -1000

    working on the brains

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const containerElement = document.getElementById('game_container');
 const names = document.getElementById('names');
-const initialButtons = document.getElementById('initial_buttons')
+const initialButtons = document.getElementById('start_controls')
+const double = document.getElementById('double');
+const triple = document.getElementById('triple');
 
 let arrayOfColumns = [];
 let arrayOfNames = [];
@@ -33,6 +35,73 @@ const addPlayer = () => {
 }
 
 
+
+
+const playerObjectsArrayMaker = array => {
+    let arrayOfOjects = [];
+    array.forEach((el) => {
+        arrayOfOjects.push({name: el, score: 0})
+    });
+    return arrayOfOjects;
+}
+
+const game = {
+    players: [],
+    multipier: 1,
+    doubleToggle: 0,
+    tripleToggle: 0,
+    double() {
+        console.log(this.players);
+        if (this.doubleToggle === 0 && this.tripleToggle === 1) {
+            this.multipier = 2;
+            this.doubleToggle = 1;
+            this.tripleToggle = 0;
+            double.style.background = "red";
+            triple.style.background = "maroon";
+        } else if (this.doubleToggle === 1 && this.tripleToggle === 0) {
+            this.multipier = 1;
+            this.doubleToggle = 0;
+            double.style.background = "maroon";
+        } else {
+            this.multipier = 2;
+            this.doubleToggle = 1;
+            double.style.background = 'red';
+        }
+        
+    },
+    triple() {
+        console.log(this.players);
+        if (this.tripleToggle === 0 && this.doubleToggle === 1) {
+            this.multipier = 3;
+            this.doubleToggle = 0;
+            this.tripleToggle = 1;
+            double.style.background = "maroon";
+            triple.style.background = "red";
+        } else if (this.tripleToggle === 1 && this.doubleToggle === 0) {
+            this.multipier = 1;
+            this.tripleToggle = 0;
+            triple.style.background = "maroon";
+        } else {
+            this.multipier = 3;
+            this.tripleToggle = 1;
+            triple.style.background = "red";
+        }
+    },
+
+    startNewGame() {
+        let turn = 0;
+        let playerLength = this.players.length
+        let curPlayer = 
+    }
+}
+
+double.addEventListener("mousedown", e => {
+    game.double();
+});
+triple.addEventListener("mousedown", e => {
+    game.triple();
+});
+
 const startGame = () => {
     let inHTML = '';
     arrayOfColumns.forEach((el) => {
@@ -40,5 +109,5 @@ const startGame = () => {
     })
     containerElement.innerHTML = inHTML
     initialButtons.innerHTML = '';
-    names.innerHTML = '';
+    game.players = playerObjectsArrayMaker(arrayOfNames);
 }
